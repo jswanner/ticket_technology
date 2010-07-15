907d130a1892cfaca4c8f1661225666b4dff83a3
Jacob Swanner <jacob@envylabs.com>
2010-07-15 15:13:18 -0400

    no need to double slash

--- lib/ticket_technology/catalog/configuration.rb
+++ lib/ticket_technology/catalog/configuration.rb
@@ -3,7 +3,7 @@ module TicketTechnology::Catalog
     MAP_ROOT = 'http://maps.seatics.com/'.freeze
 
     def self.map_url(venue)
-      "#{MAP_ROOT}/#{URI.escape(venue.map_location)}"
+      "#{MAP_ROOT}#{URI.escape(venue.map_location)}"
     end
   end
 end
